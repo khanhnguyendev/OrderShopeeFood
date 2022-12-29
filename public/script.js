fcd678068a88adf4b00beb22bd3f10c9e9607678
Ngô Khánh Nguyên <44081478+khanhnguyendev@users.noreply.github.com>
2022-12-29 11:39:39 +0700

    origin

---
+++ public/script.js
@@ -0,0 +1,64 @@
+const socket = io()
+const messageContainer = document.getElementById('message-container')
+const logContainer = document.getElementById('log-container')
+const roomContainer = document.getElementById('room-container')
+const messageForm = document.getElementById('send-container')
+const orderButton = document.querySelectorAll('#button-add')
+let orderDetail = '';
+let userName = '';
+
+// send food oder detail
+function sendOrder(event) {
+  // message = JSON.stringify({
+  //   orderUser: userName,
+  //   foodName: event.getAttribute('data-title'),
+  //   foodPrice: event.getAttribute('data-price')
+  // });
+  orderDetail = userName + ' đã đặt ' + event.getAttribute('data-title') + ' x ' + event.getAttribute('data-price')
+}
+
+if (messageForm != null) {
+  let name = prompt('What is your name?')
+  userName = name;
+  appendLog('You joined')
+  socket.emit('new-user', roomName, name)
+
+  messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    appendMessage(orderDetail)
+    socket.emit('send-chat-message', roomName, name, orderDetail)
+  })
+}
+
+socket.on('room-created', room => {
+  const roomElement = document.createElement('div')
+  roomElement.innerText = room
+  const roomLink = document.createElement('a')
+  roomLink.href = `/${room}`
+  roomContainer.append(roomElement)
+  roomContainer.append(roomLink)
+})
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.message}`)
+})
+
+socket.on('user-connected', name => {
+  appendLog(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+  appendLog(`${name} disconnected`)
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
+
+function appendLog(log) {
+  const logElement = document.createElement('div')
+  logElement.innerText = log
+  logContainer.append(logElement)
+}
---
+++ server.js
@@ -0,0 +1,113 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const port = '3000';
+const io = require('socket.io')(server)
+const Nightmare = require('nightmare')
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+let shopUrl = '';
+var foodsData = [];
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({ extended: true }))
+
+const rooms = {}
+
+app.get('/', (req, res) => {
+  res.render('index', { rooms: rooms })
+})
+
+app.post('/room', (req, res) => {
+  // Create room with shop URL
+  if (rooms[req.body.orderShopName] != null) {
+    return res.redirect('/')
+  }
+  rooms[req.body.orderShopName] = { users: {} }
+  shopUrl = req.body.orderShopUrl;
+  res.redirect(req.body.orderShopName)
+  // Send message that new room was created
+  io.emit('room-created', req.body.orderShopName)
+})
+
+app.get('/:room', (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/')
+  }
+  crawlerShopeeFood(req, res);
+})
+
+io.on('connection', socket => {
+  socket.on('new-user', (room, name) => {
+    socket.join(room)
+    rooms[room].users[socket.id] = name
+    socket.to(room).broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', (room, name, message) => {
+    socket.to(room).broadcast.emit('chat-message', { room: room, name: name, message: message, })
+  })
+  socket.on('disconnect', () => {
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id]
+    })
+  })
+
+  /**
+   * Send order detail
+   */
+  socket.on('send-order', (orderDetail) => {
+    // Saving order detail to file
+    fs.appendFile(__dirname + '/data/order.json', JSON.stringify(orderDetail), function (err) {
+      if (err) throw err;
+      console.log(orderDetail);
+      console.log('Saved!');
+    });
+  })
+})
+
+server.listen(port, function () {
+  console.log("Server is running ");
+  console.log("http://localhost:" + port);
+});
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
+
+function crawlerShopeeFood(req, res) {
+  const nightmare = Nightmare({ show: false })
+  nightmare
+    .goto(shopUrl)
+    .wait('body')
+    .wait('.menu-restaurant-list')
+    .evaluate(() => document.querySelector('div#restaurant-item').innerHTML)
+    .end()
+    .then(response => {
+      getData(response);
+    }).catch(err => {
+      console.log(err);
+    });
+
+  let getData = html => {
+    const $ = cheerio.load(html);
+    $('.item-restaurant-row').each((i, elem) => {
+      let item = {
+        title: $(elem).find('.item-restaurant-info > .item-restaurant-name').text(),
+        image: $(elem).find('img').attr('src'),
+        des: $(elem).find('div.item-restaurant-desc').text(),
+        price: $(elem).find('div.current-price').text()
+      }
+      foodsData.push(item)
+    })
+    console.log(shopUrl)
+    console.log("Crawling data complete...")
+    res.render('room', { roomName: req.params.room, foods: foodsData })
+  }
+}
